89b03241af317780b4b630a6fa1a5c78a6426284
lovekush18 <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-03-05 11:14:01 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (90.11%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,37 +1,44 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n  = nums.length;
-        if(n==1){
+         int n  = nums.length;
+          if(n==1) return nums[0];
+         if(nums[0]!=nums[1]){
             return nums[0];
-        }
-        if(nums[0]!=nums[1]){
-            return nums[0];
-        }
-        if(nums[n-1]!=nums[n-2]){
+         }
+         if(nums[n-1]!=nums[n-2]){
             return nums[n-1];
-        }
-        int low=0,high=n-1;
+         }
+         
+        int low = 1, high = n-2;
         while(low<=high){
             int mid = low+(high-low)/2;
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
-            int first = mid, second=mid;
-            if(nums[mid]==nums[mid-1]){
-                first = mid-1;
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
             else{
-                second = mid+1;
-            }
-            int leftcount = first-low, secondcount = high-second;
-            if(leftcount%2!=0){
-                high = first-1;
-            }
-            else{
-                low = second+1;
+                if(nums[mid]==nums[mid-1]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
         return -1;
-        
     }
 }
+// for(int i=1;i<n-1;i++){
+        //     if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return -2;
+        
