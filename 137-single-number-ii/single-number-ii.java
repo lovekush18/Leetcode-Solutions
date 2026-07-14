3146bd63c3477c0890d48eff5898c654c7ab59c5
lovekush18 <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-07-14 11:13:28 +0530

    Time: 6 ms (42.34%) | Memory: 46.7 MB (5.31%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,13 +1,28 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // int n = nums.length;
+        // for(int i=0;i<n-1;i+=3){
+        //     if(nums[i]!=nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[n-1];
+        HashMap<Integer,Integer> map = new HashMap<>();
         int n = nums.length;
-        for(int i=0;i<n-1;i+=3){
-            if(nums[i]!=nums[i+1]){
-                return nums[i];
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                int count = map.get(nums[i]);
+                    map.put(nums[i],count+1);
+                
+            }
+            else{
+                map.put(nums[i],1);
             }
         }
-        return nums[n-1];
-        
+        for(int i=0;i<n;i++){
+        if(map.get(nums[i])==1) return nums[i];
+    }
+    return -1;
     }
 }
