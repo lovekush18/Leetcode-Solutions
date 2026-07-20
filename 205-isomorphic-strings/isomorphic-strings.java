c2914169b582e5032231c896fd9c80460e72fcbc
lovekush18 <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-07-20 15:53:33 +0530

    Time: 17 ms (40.39%) | Memory: 43.7 MB (86.86%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -8,24 +8,21 @@ class Solution {
         for(int i=0;i<m;i++){
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-            if (map1.containsKey(c1)) {
-                if (map1.get(c1) != c2)
+            if(!map1.containsKey(c1)){
+                map1.put(c1,c2);
+            }
+            else{
+                if(map1.get(c1)!=c2){
                     return false;
                 }
-                 else {
-                    map1.put(c1, c2);
-                 }
-
-            if (map2.containsKey(c2)) {
-
-              if (map2.get(c2) != c1){
-                return false;
-              }
-
-            } 
-            else {
-
-               map2.put(c2, c1);
+            }
+            if(!map2.containsKey(c2)){
+                map2.put(c2,c1);
+            }
+            else{
+                if(map2.get(c2)!=c1){
+                    return false;
+                }
             }
         }
         return true;
