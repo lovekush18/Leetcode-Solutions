8c827469dd05a41ada80c70f24b2fa87e118ae0c
lovekush18 <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-07-30 04:23:57 +0530

    Time: 1 ms (96.64%) | Memory: 42.4 MB (74.03%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,8 +1,21 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n > 0 && (n & (n-1)) ==0)
-        return true;
+        if(n<=0){
+            return false;
+        }
+        while(n%2==0){
+            n = n/2;
+        }
+        if(n==1) return true;
         return false;
+
+
+
+        // if(n<=0) return false;
+        // if(n==1) return true;
+        // if((n & (n-1)) ==0)
+        // return true;
+        // return false;
         
     }
 }
