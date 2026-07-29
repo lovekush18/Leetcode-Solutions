b679b7fafeea7a00ad6bc9b9f52891a8e1dd34bf
lovekush18 <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-07-30 04:22:25 +0530

    Time: 1 ms (89.66%) | Memory: 42.9 MB (15.65%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n > 0 && (n & (n-1))  == 0 && (n-1)%3==0) return true;
-       
-        
+        if(n<=0) return false;
+        while(n%4==0){
+            n = n/4;
+        }
+        if(n==1) return true;
         return false;
     }
 }
