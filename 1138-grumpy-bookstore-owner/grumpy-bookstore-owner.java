d8d22a42e022f0498bdc4908f36d4c8c22a202a4
lovekush18 <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-08-04 01:45:14 +0530

    Time: 3 ms (96.07%) | Memory: 48.1 MB (98.82%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxSatisfied(int[] c, int[] g, int k) {
+        int m = c.length;
+        int n = g.length;
+        int sum = 0;
+       
+        for(int i = 0;i<n;i++){
+            if(g[i]==0){
+                sum+=c[i];
+            }
+        }
+        int maxsum = sum;
+        for(int i = 0;i<k;i++){
+            if(g[i]==1){
+                sum = sum+c[i];
+                maxsum =sum;
+            }
+        }
+        for(int i = k;i<n;i++){
+            if(g[i]==1){
+                sum = sum+c[i];
+            }
+            if(g[i-k]==1){
+                sum = sum-c[i-k];
+            }
+            maxsum = Math.max(maxsum,sum);
+            
+        }
+        return maxsum;
+
+    }
+}
