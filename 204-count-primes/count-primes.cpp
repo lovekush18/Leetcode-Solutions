ce7f382adb3b2b631541618042c1fcdd9e714daa
Lovekush Goyal <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-08-08 23:12:44 +0530

    Time: 248 ms (13.02%) | Memory: 12.6 MB (91.73%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int countPrimes(int n) {
         vector<bool> arr(n+1);
-        for(int i = 0;i<n;i++){
+        for(int i = 2;i<n;i++){
             arr[i]=true;
         }
-        arr[0] = false;
-        arr[1] = false;
+        
         for(int i = 2;i*i<n;i++){
             if(arr[i]==true){
                 for(int j = i*i;j<n;j+=i){
