93bb9f4e38b89ff2cf461c315e7da9ed7b227c24
Lovekush Goyal <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-08-22 00:34:46 +0530

    Time: 1 ms (95.21%) | Memory: 42 MB (80.28%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
+        if(num<=1) return false;
         int sum = 0;
-        for(int i=1;i<num;i++){
+        for(int i=2;i*i<num;i++){
             if(num%i==0){
             sum+=i;
+                if(i!=num/i){
+                    sum+=num/i;
+                }
             }
             
         }
-        if(sum==num) return true;
+        if((sum+1)==num) return true;
        
         return false;
         
