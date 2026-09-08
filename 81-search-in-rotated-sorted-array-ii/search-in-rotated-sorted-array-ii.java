226339d39c291e4b4d19da93e391e534c780f4d4
Lovekush Goyal <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-09-08 11:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (65.60%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -17,7 +17,7 @@ class Solution {
             }
 
             else if(nums[low]<=nums[mid]){
-                if(nums[low] <= target && target < nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
                     high = mid-1;
                 }
                 else{
@@ -26,7 +26,7 @@ class Solution {
             }
             
             else if(nums[mid]<=nums[high]){
-                    if(nums[mid] <target && target <= nums[high]){
+                    if(nums[mid] <= target && target <= nums[high]){
                         low = mid+1;
                     }
                     else{
