97f76c11921197c183d9e93d7ef49c3c83f33df1
Lovekush Goyal <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-09-14 14:30:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (15.09%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return 0;
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            if(i==0){
+                if(nums[i]>nums[i+1]){
+                    ans = i;
+                    break;
+                }
+            }
+            else if(i==n-1){
+                if(nums[i]>nums[i-1]){
+                    ans = i;
+                    break;
+                }
+            }
+            else{
+                if(nums[i]>nums[i+1] && nums[i]>nums[i-1]){
+                    ans = i;
+                    break;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
