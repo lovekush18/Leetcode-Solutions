64a88485c2893beecf7f6545c9d9e37243863155
Lovekush Goyal <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-09-14 13:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (76.71%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,60 +1,79 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // int n  = nums.length;
-        //  if(n==1) return nums[0];
-        //  if(nums[0]!=nums[1]){
-        //     return nums[0];
-        //  }
-        //  if(nums[n-1]!=nums[n-2]){
-        //     return nums[n-1];
-        //  }
-        //   // is type ke que m agr single element exist krega to array ki length hamesha odd hogi
-         
-        // int low = 0, high = n-1;
-        // while(low<=high){
-        //     int mid = low+(high-low)/2;
-        //     if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-        //         return nums[mid];
-        //     }
-        //     else if(mid%2==0){ // mid agr even hua to uske left m size even aur right m size even
-        //         if(nums[mid]==nums[mid+1]){
-        //             low = mid+1;
-        //         }
-        //         // agr mid even h to uske right side dekhenge 
-        //         else{
-        //             high = mid-1;
-        //         }
-        //     }
-        //     else{
-        //         if(nums[mid]==nums[mid-1]){
-        //             low = mid+1;
-        //         }
-        //         // agr mid odd h to uske left side dekhenge 
-        //         else{
-        //             high = mid-1;
-        //         }
-        //     }
-        // }
-        // return -1;
         int n  = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i =0;i<n;i++){
-            if(map.containsKey(nums[i])){
-                int c = map.get(nums[i]);
-                map.put(nums[i],c+1);
+         if(n==1) return nums[0];
+         if(nums[0]!=nums[1]){
+            return nums[0];
+         }
+         if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+         }
+          // is type ke que m agr single element exist krega to array ki length hamesha odd hogi
+         
+        int low = 0, high = n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            else if(mid%2==0){ 
+                if(nums[mid]==nums[mid+1]){
+                    low = mid+1;
+                }
+                
+                else if(nums[mid]==nums[mid-1]){
+                    high = mid-1;
+                }
             }
             else{
-                map.put(nums[i],1);
+                if(nums[mid]==nums[mid-1]){
+                    low = mid+1;
+                }
+                
+                else if(nums[mid]==nums[mid+1]){
+                    high = mid-1;
+                }
             }
-        }
-        ArrayList<Integer> list = new ArrayList<>(map.keySet());
-        for(int i = 0;i<list.size();i++){
-            int ele = list.get(i);
-            int val = map.get(ele);
-            if(val==1) return ele;
         }
         return -1;
 
+        // HashMap Approach
+
+        // int n  = nums.length;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i =0;i<n;i++){
+        //     if(map.containsKey(nums[i])){
+        //         int c = map.get(nums[i]);
+        //         map.put(nums[i],c+1);
+        //     }
+        //     else{
+        //         map.put(nums[i],1);
+        //     }
+        // }
+        // ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        // for(int i = 0;i<list.size();i++){
+        //     int ele = list.get(i);
+        //     int val = map.get(ele);
+        //     if(val==1) return ele;
+        // }
+        // return -1;
+
+        // HashSet Approach
+        
+        // int n  = nums.length;
+        // HashSet <Integer> set = new HashSet<>();
+        // for(int i = 0;i<n;i++){
+        //     if(set.contains(nums[i])){
+        //         set.remove(nums[i]);
+        //     }
+        //     else{
+        //         set.add(nums[i]);
+        //     }
+        // }
+        // ArrayList<Integer> list = new ArrayList<>(set);
+        
+        // return list.get(0);
+   
     }
 }
 
