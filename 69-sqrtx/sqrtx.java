ec056325657c0a4eca8d58074dbef39fd83ab337
Lovekush Goyal <lovekush.24scse1011513@galgotiasuniversity.ac.in>
2026-09-16 16:45:41 +0530

    Time: 1 ms (99.28%) | Memory: 42.5 MB (78.76%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+       
+        int l = 1 , h = x;
+        int ans = 0;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            long val = (long)mid*mid;
+            if(val<=x){
+                ans = mid;
+                l = mid+1;
+            }
+            else{
+                h = mid-1;
+            }
+        }
+        return ans;
+            
+        
+    }
+}
